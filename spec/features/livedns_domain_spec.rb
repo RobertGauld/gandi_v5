e161c7e9474dd3590a484a9a40f438d1da622dc6
Robert Gauld <robert@robertgauld.uk>
2020-07-05 10:13:56 +0100

    Remove feature specs that just repeat unit specs

--- spec/features/livedns_domain_spec.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-describe 'LiveDNS Domain features' do
-  it 'List domains', :vcr do
-    list = GandiV5::LiveDNS::Domain.list
-    expect(list.map(&:fqdn)).to match_array %w[example.com example.net]
-  end
-end
